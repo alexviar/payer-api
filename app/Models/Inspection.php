bd6cd2a1beabe9eae46ed05521006c1fbd2000df
alexviar <alexanderviar@gmail.com>
2025-04-21 11:03:45 -0400

    feat(inspections): add product relationship to Inspection model

--- app/Models/Inspection.php
+++ app/Models/Inspection.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Inspection extends Model
 {
     /** @use HasFactory<\Database\Factories\InspectionFactory> */
     use HasFactory;
+
+    #region Relations
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    #endregion
 }
