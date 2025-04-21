60eff43bbd08b12a39996cff9e602c1d30adb548
alexviar <alexanderviar@gmail.com>
2025-04-21 12:18:05 -0400

    feat(inspections): implement listing functionality

--- app/Http/Controllers/InspectionController.php
+++ app/Http/Controllers/InspectionController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Inspection;
 use Illuminate\Http\Request;
 
 class InspectionController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $query = Inspection::query();
+
+        return $query->paginate($request->input('page_size'));
+    }
 }
