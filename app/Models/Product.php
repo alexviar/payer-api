e5e982ae8ee520989915539b02948e8cec272c54
alexviar <alexanderviar@gmail.com>
2025-04-21 11:04:27 -0400

    feat(product): add relationship to CustomAttribute model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+
+    #region Relations
+
+    public function attributes(): BelongsToMany
+    {
+        return $this->belongsToMany(CustomAttribute::class, 'product_attributes')
+            ->withTimestamps();
+    }
+
+    #endregion
 }
