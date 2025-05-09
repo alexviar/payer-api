4dd73cc780a3b1d4381b92384d06b09a7f28b4dd
alexviar <alexanderviar@gmail.com>
2025-05-09 15:48:10 -0400

    feat(defects): add list functionality with pagination

--- app/Http/Controllers/DefectController.php
+++ app/Http/Controllers/DefectController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Defect;
 use Illuminate\Http\Request;
 
 class DefectController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $query = Defect::query();
+
+        /** @var LengthAwarePaginator $result */
+        $result = $query->paginate($request->input('page_size'));
+
+        return $result;
+    }
 }
