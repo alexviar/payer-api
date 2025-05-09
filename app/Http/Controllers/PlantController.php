2e80b43a6ae39f414d7027ad96878eb1d283fd98
alexviar <alexanderviar@gmail.com>
2025-05-09 15:56:33 -0400

    feat(plants): add list functionality with pagination

--- app/Http/Controllers/PlantController.php
+++ app/Http/Controllers/PlantController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Plant;
 use Illuminate\Http\Request;
 
 class PlantController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $query = Plant::query();
+
+        /** @var LengthAwarePaginator $result */
+        $result = $query->paginate($request->input('page_size'));
+
+        return $result;
+    }
 }
