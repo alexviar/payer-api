f99bbe7dae7108602c74d5035bb22d883287509d
alexviar <alexanderviar@gmail.com>
2025-05-09 15:58:36 -0400

    feat(sales-agents): add list functionality with pagination

--- app/Http/Controllers/SalesAgentController.php
+++ app/Http/Controllers/SalesAgentController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SalesAgent;
 use Illuminate\Http\Request;
 
 class SalesAgentController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $query = SalesAgent::query();
+
+        /** @var LengthAwarePaginator $result */
+        $result = $query->paginate($request->input('page_size'));
+
+        return $result;
+    }
 }
