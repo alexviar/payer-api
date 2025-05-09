748b6d074ab020c5500f45fddb608869e99d5b74
alexviar <alexanderviar@gmail.com>
2025-05-09 15:52:20 -0400

    feat(reworks): add search functionality

--- app/Http/Controllers/ReworkController.php
+++ app/Http/Controllers/ReworkController.php
@@ -3,13 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Rework;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class ReworkController extends Controller
 {
+
+    public function applyFilters(Request $request, Builder $query)
+    {
+        if ($request->has('search')) {
+            $query->where('name', 'like', "%$request->search%");
+        }
+    }
+
     public function index(Request $request)
     {
         $query = Rework::query();
+
+        $this->applyFilters($request, $query);
 
         /** @var LengthAwarePaginator $result */
         $result = $query->paginate($request->input('page_size'));
