00a495d460e79ad6d6a1df0e459250b51b39cd94
alexviar <alexanderviar@gmail.com>
2025-05-09 14:14:25 -0400

    feat(custom-attributes): add listing with search and pagination and create functionality

--- app/Http/Controllers/CustomAttributeController.php
+++ app/Http/Controllers/CustomAttributeController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CustomAttribute;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class CustomAttributeController extends Controller
 {
-    //
+    public function applyFilters(Request $request, Builder $query)
+    {
+        if ($request->has('search')) {
+            $query->where('name', 'like', "%$request->search%");
+        }
+    }
+
+    public function index(Request $request)
+    {
+        $query = CustomAttribute::query();
+
+        $this->applyFilters($request, $query);
+
+        /** @var LengthAwarePaginator $result */
+        $result = $query->paginate($request->input('page_size'));
+
+        return $result;
+    }
+
+    public function store(Request $request)
+    {
+        $attribute = CustomAttribute::create($request->all());
+        return $attribute;
+    }
 }
--- app/Models/CustomAttribute.php
+++ app/Models/CustomAttribute.php
@@ -9,4 +9,14 @@ class CustomAttribute extends Model
 {
     /** @use HasFactory<\Database\Factories\CustomAttributeFactory> */
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+    ];
 }
