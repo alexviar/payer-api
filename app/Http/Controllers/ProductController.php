df79b0a653c72f84396d0cb6e115847acd51f65a
alexviar <alexanderviar@gmail.com>
2025-05-09 14:27:24 -0400

    feat(products): add listing with pagination and search functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function applyFilters(Request $request, Builder $query)
+    {
+        if ($request->has('search')) {
+            $query->where('name', 'like', "%$request->search%");
+        }
+    }
+
+    public function index(Request $request)
+    {
+        $query = Product::query();
+
+        $this->applyFilters($request, $query);
+
+        /** @var LengthAwarePaginator $result */
+        $result = $query->paginate($request->input('page_size'));
+
+        return $result;
+    }
 }
