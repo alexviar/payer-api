2b4ffa8edc6ff436cae4f4a673d9b094d4035941
alexviar <alexanderviar@gmail.com>
2025-05-12 06:29:54 -0400

    test: enable RefreshDatabase and add fake seed in Pest.php
    Enable the RefreshDatabase trait in Pest.php to ensure a fresh database state for each test. Additionally, add a beforeEach hook to seed the database with a fixed seed value (71937) for consistent test data.

--- tests/Pest.php
+++ tests/Pest.php
@@ -12,7 +12,10 @@
 */
 
 pest()->extend(Tests\TestCase::class)
- // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+    ->beforeEach(function () {
+        fake()->seed(71937);
+    })
     ->in('Feature');
 
 /*
