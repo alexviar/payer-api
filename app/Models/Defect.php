fe63ac31c0cfa0d685e192da21ef2688e5490383
alexviar <alexanderviar@gmail.com>
2025-05-27 13:34:50 -0400

    add missing lot relationship

--- app/Models/Defect.php
+++ app/Models/Defect.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Defect extends Model
 {
@@ -13,4 +14,9 @@ class Defect extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function lot(): BelongsTo
+    {
+        return $this->belongsTo(InspectionLot::class, 'inspection_lot_id');
+    }
 }
