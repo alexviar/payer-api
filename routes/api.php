d72c28345dbe885862dee4b8f1161d5f6cba2e39
alexviar <alexanderviar@gmail.com>
2025-05-30 14:32:37 -0400

    feat: load user settings

--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,11 @@ Route::controller(AuthController::class)->prefix('auth')->group(function () {
     Route::post('change-password', 'changePassword')->middleware('auth:sanctum');
     Route::post('reset-password', 'resetPassword')->name('password.reset');
     Route::post('forgot-password', 'forgotPassword');
-    Route::get('user', fn(Request $request) => $request->user())->middleware('auth:sanctum');
+    Route::get('user', function (Request $request) {
+        $user = $request->user();
+        $user->load('settings');
+        return $user;
+    })->middleware('auth:sanctum');
     Route::patch('profile', 'updateProfile')->middleware('auth:sanctum');
     Route::patch('settings', 'updateSettings')->middleware('auth:sanctum');
 });
